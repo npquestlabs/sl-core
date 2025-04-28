3d2cf9faa462558f50b8dd1699d28552bb55a16b
Isaac <nisaacdz@gmail.com>
2025-04-28 02:55:06 +0000

    Add user type to request type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,9 +1,9 @@
-import { User } from '../../../generated/prisma';
+import { User } from '../../../generated/prisma'
 
 declare global {
   namespace Express {
     interface Request {
-      user?: User;
+      user?: User
     }
   }
 }
