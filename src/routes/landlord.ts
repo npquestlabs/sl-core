06602c0d1256fd83110df049e6fa7b1c3684f629
Isaac <nisaacdz@gmail.com>
2025-04-28 02:55:24 +0000

    Add sample routes

--- src/routes/landlord.ts
+++ src/routes/landlord.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import {
-  createLandlordStep1,
-  createLandlordStep2,
-} from '../controllers/landlord'
+import { createLandlord, setLandLordData } from '../controllers/landlord'
+import { setUserData } from '../controllers/user'
+import { authenticate } from '../middlewares/authenticate'
 
 const router = Router()
 
-router.post('/register/step1', createLandlordStep1)
-router.put('/register/step2/:id', createLandlordStep2)
+router.post('/register/step1', createLandlord)
+router.post('/register/step2', authenticate, setUserData)
+router.put('/register/step3', authenticate, setLandLordData)
 
 export default router
