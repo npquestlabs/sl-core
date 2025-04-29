6ae515d9ca7c9ffe1fe032fe366d7804ffaf1e15
Isaac <nisaacdz@gmail.com>
2025-04-29 15:00:20 +0000

    Renamed authenticate.ts to auth.middleware.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authenticate } from '../middlewares/authenticate'
+import { authenticate } from '../middlewares/auth.middleware'
 import * as authController from '../controllers/auth.controller'
 
 const router = express.Router()
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authenticate } from '../middlewares/authenticate'
+import { authenticate } from '../middlewares/auth.middleware'
 import * as userController from '../controllers/user.controller'
 
 const router = express.Router()
