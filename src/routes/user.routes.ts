91842371b728d75096b773bf63f062cdcb5bf77e
Isaac <nisaacdz@gmail.com>
2025-05-02 00:54:51 +0000

    Add bodyvalidator middleware to user update

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { authenticate } from '../middlewares/auth.middleware'
 import * as userController from '../controllers/user.controller'
+import { validateBody } from '../middlewares/validator.middleware'
+import { UpdateUserSchema } from '../schemas/user.schema'
 
 const router = express.Router()
 
 router.get('/me', authenticate, userController.getUser)
-router.patch('/me/update', authenticate, userController.updateUser)
+router.patch('/me/update', authenticate, validateBody(UpdateUserSchema), userController.updateUser)
 
 export default router
