72d2ee7a8b5a6d6053710953ba58175f426c8859
Isaac <nisaacdz@gmail.com>
2025-07-31 07:30:35 +0000

    update bouncer middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,14 +3,16 @@ import { logger } from '../configs/logger'
 import envConfig from '../configs/environment'
 
 export function bouncer(req: Request, res: Response, next: NextFunction) {
-  const origin = req.get('origin') || 'not-set'
-  const client = req.headers['x-client'] || 'not-set'
-  const permittedOrigin = envConfig.allowedOrigins.includes(origin)
+  const origin = req.get('origin')
+  const client = req.headers['x-client']
+  const permittedOrigin = envConfig.allowedOrigins.includes(origin || 'not-set')
   const permittedClient =
     typeof client == 'string' && client in envConfig.clients
 
   if (!permittedOrigin || !permittedClient) {
-    logger.warn(`Blocked request with client: ${client} from origin: ${origin}`)
+    logger.warn(
+      `Blocked request with client: "${client}" from origin: "${origin}"`,
+    )
     return res.status(403).json({ error: 'Permission denied' })
   }
 
